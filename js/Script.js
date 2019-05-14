ef0f9330f80ac27725c0fecd8406b9dcae253f03
Fuad Hajiyev <hajiyev1996@gmail.com>
2019-05-14 13:02:20 +0400

    commit edib push edecem

---
+++ js/Script.js
@@ -0,0 +1,13 @@
+function ani() {
+
+    document.getElementById('box').style.display = 'block';
+    document.getElementById('box').style.animationPlayState = 'running';
+}
+
+
+function ani2() {
+
+
+    document.getElementById('box').style.animationPlayState = 'paused';
+
+}
